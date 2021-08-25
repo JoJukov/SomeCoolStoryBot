07bdbdf2e329de65dc0bdc40c3b8bb65615a2a5c
iliayar <iliayar3@gmail.com>
2021-08-25 22:27:22 +0300

    Add fetching styles

--- balaboba/_balaboba.py
+++ balaboba/_balaboba.py
@@ -1,11 +1,26 @@
 import requests
 
+DISCLAIMER = '''
+Балабоба не принимает запросы на острые темы,
+например, про политику или религию. Люди могут слишком серьёзно
+отнестись к сгенерированным текстам.
+
+Вероятность того, что запрос задаёт одну из острых тем, определяет
+нейросеть, обученная на оценках случайных людей. Но она может
+перестараться или, наоборот, что-то пропустить.
+'''
+
 NO_STYLE = 1
 
-class BalabobaGenerateException(Exception):
+class BalabobaRequestFailedException(Exception):
 
     def __init__(self, message) -> None:
         super().__init__(f'Failed to get generated text: {message}')
+
+class BalabobaInvalidQueryException(Exception):
+
+    def __init__(self, *args: object) -> None:
+        super().__init__(DISCLAIMER, *args)
 
 class Style:
     '''
@@ -13,13 +28,19 @@ class Style:
 
     Attributes:
     -----------------
+    `name` : `str`
+        Short description of style option
     `description` : `str`
-        Field contains name of style
+        Long description of style option
     '''
 
-    def __init__(self, description, id):
-        self._id = id
+    def __init__(self, _id, name, description):
+        self._id = _id
+        self.name = name
         self.description = description
+
+    def __str__(self):
+        return self.name
 
 class Balaboba:
     '''
@@ -32,8 +53,11 @@ class Balaboba:
     '''
 
     API_URL   = "https://yandex.ru/lab/api/yalm"
-    STYLE_URL = "/intros"
-    QUERY_URL = "/text3"
+    STYLE_URL = "intros"
+    QUERY_URL = "text3"
+
+    def __init__(self) -> None:
+        pass
 
     def getStyles(self):
         '''
@@ -45,17 +69,27 @@ class Balaboba:
 
         Throws
         ---------------
-        `BalabobaFetchStylesException`
+        `BalabobaRequestFailedException`
             When face an error getting or parsing response
         '''
-        return 
 
-    def __init__(self) -> None:
-        pass
+        jsonResponse = self._checkedGet(
+            [
+                "intros",
+            ],
+            self._styleUrl(),
+        )
+
+        intros = jsonResponse["intros"]
+
+        for intro in intros:
+            if len(intro) < 3:
+                raise BalabobaRequestFailedException("Some styles contas too few parameters")
+
+        return self._processIntros(intros)
 
     def generate(self, query, style = NO_STYLE):
-        '''
-        Abusing *Balaboba* to generate text.
+        '''Abusing *Balaboba* to generate text.
 
         Parameters
         ---------------
@@ -70,25 +104,75 @@ class Balaboba:
 
         Throws
         ----------------
-        `BalabobaGenerateException`
+        `BalabobaRequestFailedException`
             When face an error getting or parsing response
+        `BalabobaInvalidQueryException`
+            Read DISLAIMER
         '''
 
-        response = requests.post(
+        jsonResponse = self._checkedPost(
+            [
+                "query",
+                "text",
+            ],
             self._queryUrl(),
             json=self._createQueryJsonRequest(query, style),
-        ).json()
-        return self._jsonResponseToText(response)
+        )
+
+        if jsonResponse["bad_query"] == 1:
+            raise BalabobaInvalidQueryException()
+
+        return self._jsonResponseToText(jsonResponse)
+
+    def _checkedRequest(self, method, checkFields, url, *args, **kwargs):
+        try:
+            response = method(url, *args, **kwargs);
+        except:
+            raise BalabobaRequestFailedException(f'Request to "{self._queryUrl()}" failed')
+
+        try:
+            jsonResponse = response.json()
+        except:
+            raise BalabobaRequestFailedException(f'Failed to parse to json: "{response.text}"')
+
+        for field in checkFields:
+            if jsonResponse.get(field) == None:
+                raise BalabobaRequestFailedException(f'No "{field}" value in response')
+
+        if jsonResponse["error"] == 1:
+            raise BalabobaRequestFailedException("Balaboba failed to process request")
+
+        return jsonResponse
+
+    def _checkedPost(self, *args, **kwargs):
+        return self._checkedRequest(requests.post, *args, **kwargs)
+
+    def _checkedGet(self, *args, **kwargs):
+        return self._checkedRequest(requests.get, *args, **kwargs)
 
     def _createQueryJsonRequest(self, query, style):
         return {
             "query": query,
-            "intro": style,
+            "intro": style._id,
             "filter": 0,
         }
 
     def _jsonResponseToText(self, response):
         return response["query"] + response["text"]
+
+    def _processIntros(self, intros):
+        styles = []
+
+        for intro in intros:
+            style = Style(
+                intro[0],
+                intro[1],
+                intro[2],
+            )
+
+            styles.append(style)
+
+        return styles
 
     def _methodUrl(self, method):
         return f'{self.API_URL}/{method}'
@@ -97,4 +181,4 @@ class Balaboba:
         return self._methodUrl(self.STYLE_URL)
     
     def _queryUrl(self):
-        return self._methodUrl(self.API_URL)
+        return self._methodUrl(self.QUERY_URL)
